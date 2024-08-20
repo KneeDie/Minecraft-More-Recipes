850c505e7d76ae345a0ac780bb7515164a089a47
郑英弘毅 郑 <HYweiruan@hotmail.com>
2024-08-20 17:22:34 +0800

    v1.0_更多配方MCmod

---
+++ src/main/java/kg/us/kneedie/more_recipes/MoreRecipesDataGenerator.java
@@ -0,0 +1,11 @@
+package kg.us.kneedie.more_recipes;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MoreRecipesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
